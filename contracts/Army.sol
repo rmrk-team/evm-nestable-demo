7fdfddc0753538d70c964e2bbe218283a748fdd1
dev-andreavendrame <dev.andreavendrame@gmail.com>
2022-11-23 23:22:37 +0100

    added contracts for basic user journey

---
+++ contracts/Army.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "@rmrk-team/evm-contracts/contracts/implementations/RMRKNestableImpl.sol";
+
+contract Army is RMRKNestableImpl {
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 maxSupply,
+        uint256 pricePerMint,
+        string memory collectionMetadata,
+        string memory tokenURI,
+        address royaltyRecipient,
+        uint256 royaltyPercentageBps
+    ) RMRKNestableImpl(
+        name,
+        symbol,
+        maxSupply,
+        pricePerMint,
+        collectionMetadata,
+        tokenURI,
+        royaltyRecipient,
+        royaltyPercentageBps
+    ) {}
+}
